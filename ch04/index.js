1bf645c64fa6e50fa8650bb2b33119f42d0b23fd
Yeji Koo <82144134+xlzbthxyjkoo@users.noreply.github.com>
2023-03-18 17:41:37 +0900

    Create index.js

---
+++ ch04/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+setInterval(() => {
+  root.render(
+  <React.StrictMode>
+    <Clock />
+  </React.StrictMode>,
+  document.getElementById('root')
+  );
+}, 1000);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
